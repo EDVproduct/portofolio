678214dadb89190154527ad621c44823b3ab66c7
EDVproduct <31473262+EDVproduct@users.noreply.github.com>
2018-03-03 19:50:38 +0100

    Add files via upload
    I hope this time the upload will be correct.

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+    
+           $(document).ready(function(){
+             $(".mas1").click(function(){
+                 $(".hidden1").toggleClass("apear");
+             });
+             $(".mas2").click(function(){
+                 $(".hidden2").toggleClass("apear");
+             });
+             $(".mas3").click(function(){
+                 $(".hidden3").toggleClass("apear");
+             });
+             $(".mas4").click(function(){
+                 $(".hidden4").toggleClass("apear");
+             });
+           });
+  
